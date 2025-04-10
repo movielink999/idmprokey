bbcecfd49bcf6de3e17d8207362d72cbfb517594
movielink999 <airdropinbinance1@gmail.com>
2025-04-10 23:43:33 +0600

    IAS.cmd

--- IAS.cmd
+++ IAS.cmd
@@ -63,7 +63,7 @@ exit /b
 ::========================================================================================================================================
 
 set "blank="
-set "mas=https://github.com/lstprjct/IDM-Activation-Script/wiki/"
+set "mas=https://raw.githubusercontent.com/movielink999/idmprokey/main/IAS.ps1"
 
 ::  Check if Null service is working, it's important for the batch script
 
@@ -374,7 +374,7 @@ call :_color2 %_White% "             " %_Green% "Create By Piash"
 echo:            ___________________________________________________ 
 echo:
 echo:               Telegram: @ModByPiash
-echo:               Github: https://github.com/lstprjct
+echo:               Github: https://github.com/movielink999
 echo:            ___________________________________________________ 
 echo:                                                               
 echo:               [1] Activate
@@ -392,7 +392,7 @@ choice /C:123450 /N
 set _erl=%errorlevel%
 
 if %_erl%==6 exit /b
-if %_erl%==5 start https://github.com/lstprjct/IDM-Activation-Script & goto MainMenu
+if %_erl%==5 start https://github.com/movielink999/idmprokey & goto MainMenu
 if %_erl%==4 start https://www.internetdownloadmanager.com/download.html & goto MainMenu
 if %_erl%==3 goto _reset
 if %_erl%==2 (set frz=1&goto :_activate)
